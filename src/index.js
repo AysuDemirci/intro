b246271554c307cc6ceec4bbd0f2f0e9f687165d
Aysu <aysu.gunerr@gmail.com>
2022-11-10 12:46:05 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -8,21 +8,12 @@ import { BrowserRouter } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-// 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <App />
-    {/* <Routes>
-      <Route path="/*" element={<App />}></Route>
-
-      <Route path="/product" element={<SecondPage />}></Route>
-    </Routes> */}
+  <App />    
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
